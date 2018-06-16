d2be20706bfcb3b63c97943eed31131df455b5d6
haoxin <yhxlele@gmail.com>
2018-06-16 18:29:45 +0800

    Run script

--- centralServer/__init__.py
+++ centralServer/__init__.py
@@ -2,6 +2,8 @@ import flask
 
 app = flask.Flask(__name__)
 app.config.from_object('centralServer.config')
+app.config.from_envvar('SERVER_SETTINGS', silent=True)
+
 
 import centralServer.views
 import centralServer.model
---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python3 -m venv env
+source env/bin/activate
+pip install --upgrade pip
+pip install -e .
+export FLASK_DEBUG=True
+export FLASK_APP=centralServer
+export SERVER_SETTINGS=config.py
+flask run --host 0.0.0.0 --port 8000

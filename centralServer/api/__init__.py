a4229bea2b44dc9f64dd29afcf48b469f6390e9f
gaoleMeng <gaole@umich.edu>
2018-08-01 11:14:04 +0800

    new

--- centralServer/api/__init__.py
+++ centralServer/api/__init__.py
@@ -7,13 +7,13 @@ from centralServer.api.recvfile import *
 import socket
 
 
-print("socket finish")
 local_ip = socket.gethostbyname(socket.gethostname())
 values = {
     "name": "gaole",
     "ip": local_ip
 }
-print("start")
+
 req = urllib.request.Request("https://mboard-middle-server.herokuapp.com/api/sendip", json.dumps(values).encode(encoding='UTF8'), headers={'Content-type':'application/json', 'Accept':'text/plain'})
 response = urlopen(req)
+print("ip ready")
 print(response.read())

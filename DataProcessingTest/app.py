6852c815da2f823a9a5f2fc325ed8180883d6f1d
Bingxin Zhang <winizhbx@umich.edu>
2018-08-07 18:26:56 +0800

    media processing

---
+++ DataProcessingTest/app.py
@@ -0,0 +1,15 @@
+import sklearn
+from sklearn.cluster import KMeans
+
+# Number of clusters
+kmeans = KMeans(n_clusters=3)
+# Fitting the input data
+kmeans = kmeans.fit(X)
+# Getting the cluster labels
+labels = kmeans.predict(X)
+# Centroid values
+centroids = kmeans.cluster_centers_
+
+# Comparing with scikit-learn centroids
+print(C) # From Scratch
+print(centroids) # From sci-kit learn
